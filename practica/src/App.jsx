d3cf9a0eac9fa46bdf3abdfaa5c6b3dcdb2922fd
alexis coronel <80395647+SacZ16@users.noreply.github.com>
2023-02-11 20:00:44 -0300

    initial

---
+++ practica/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  // 1-Crear Un Contador
+  //2-crear un input que agrege a un estado todo lo escrito,
+  //como si fuera publicaciones
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
